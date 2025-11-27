120eb44b21bb110566985d166bbd31ecfbd55703
HarithaSudheesh <haritha601974@gmail.com>
2025-11-27 13:26:28 +0530

    Updated build.sh

--- build.sh
+++ build.sh
@@ -9,5 +9,20 @@ pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 
+# Create superuser automatically
+echo "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = 'haritha@gtec'
+email = 'harithacholayil01@gmail.com'
+password = 'hari@gtec1974'
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+" | python manage.py shell
+
 # Collect static files
 python manage.py collectstatic --no-input
